32338a7f7c52e47039d833c490dce02385d3af6f
Dayron <dayronbp06@gmail.com>
2024-11-12 16:58:58 -0400

    Fix login form bug and add key blocking for username and password fields

--- GUI/FrmLogin.vb
+++ GUI/FrmLogin.vb
@@ -1,29 +1,12 @@
 ﻿Public Class FrmLogin
     Private Sub FrmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
     End Sub
 
     Public Sub AbrirFrm(ByVal frmHijo As Form)
         If Me.PContenedor.Controls.Count > 0 Then
             Me.PContenedor.Controls.RemoveAt(0)
         End If
-    End Sub
-
-
-    Private Sub BInicioSesion_Click(sender As Object, e As EventArgs)
-
-        Dim userName As String = "Admin"
-        Dim password As String = "Papo"
-
-        If txtUserName.Text = "Admin" And txtPassword.Text = "Papo" Then
-            Me.Hide()
-            Dim frm As New frmPrincipal
-            frm.ShowDialog(Me)
-        Else
-            MessageBox.Show("Nombre de usuario o contraseña inválidos.")
-        End If
-
     End Sub
 
     Private Sub Limpiar_Click(sender As Object, e As EventArgs) Handles Limpiar.Click
@@ -40,12 +23,42 @@
         Dim userName As String = "Admin"
         Dim password As String = "Muebles"
 
-        If txtUserName.Text = "Admin" And txtPassword.Text = "Muebles" Then
-            Me.Hide()
+
+        If txtUserName.Text = userName AndAlso txtPassword.Text = password Then
             Dim frm As New frmPrincipal
-            frm.ShowDialog(Me)
+            Me.Hide()
+            frm.ShowDialog()
+            Me.Close()
         Else
             MessageBox.Show("Nombre de usuario o contraseña inválidos.")
+            txtPassword.Clear()
+            txtPassword.Focus()
         End If
     End Sub
+
+    Private Sub txtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            BInicioSesion_Click_1(sender, e) ' Llama al evento del botón cuando presionas Enter
+        End If
+    End Sub
+
+    Private Sub txtPassword_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPassword.KeyPress
+        ' Bloquea la tecla de espacio
+        If e.KeyChar = ChrW(32) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtUserName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUserName.KeyPress
+        ' Bloquea la tecla de espacio
+        If e.KeyChar = ChrW(32) Then
+            e.Handled = True
+        End If
+
+        ' Bloquear números 
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
 End Class
