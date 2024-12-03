b7eac04e961b7422af6f8d27ef82abdac512467d
Dayron <dayronbp06@gmail.com>
2024-12-03 17:14:04 -0400

    feat: add UsuariosEntity

---
+++ CapaEntidades/Usuarios.vb
@@ -0,0 +1,10 @@
+﻿Public Module SesionUsuario
+    Public UsuarioActual As Usuarios
+End Module
+Public Class Usuarios
+    Public Property UsuarioId As Integer
+    Public Property NombreUsuario As String
+    Public Property Email As String
+    Public Property Contrasena As String
+End Class
+
