af71e956757862c412eee7426b3ab9bd13e17ca6
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-04 14:21:08 -0400

    feat: add inventario service

---
+++ BLL/Services/InventarioService.vb
@@ -0,0 +1,17 @@
+﻿Imports CapaEntidades
+Imports DAL
+
+Public Class InventarioService
+
+    Private _dalInventario As New InventarioDAL()
+    Private _dalProducto As New ProductoCrud()
+
+    Public Sub InsertarProductoInventario(inventario As Inventario)
+        _dalInventario.InsertarProductoInventrario(inventario)
+    End Sub
+
+    Public Function ObtenerProducto() As List(Of Producto)
+        Return _dalProducto.ObtenerProductos()
+    End Function
+
+End Class
